252697be72f713f142b1921cfb92d44ed83563a0
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-05-11 20:16:59 +0200

    welcome layout const width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <section className=' h-[90%] w-[65%] rounded-2xl bg-white shadow-lg max-[645px]:h-[95%] max-[645px]:w-[94%]'>
+    <section className='h-[90%] w-[95%] max-w-[940px] rounded-2xl bg-white shadow-lg max-[645px]:h-[95%] max-[645px]:w-[94%]'>
       <div className='grid h-full w-full place-items-center'>
         <div className='flex h-full w-full max-w-[550px] flex-col items-center justify-center gap-7'>
           {children}
