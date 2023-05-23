972812a46e043b20c4deb970f76692138c12cc68
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-05-23 22:57:14 +0200

    calories todays progress 4 pies

---
+++ src/components/FatPie.tsx
@@ -0,0 +1,35 @@
+import { PieChart, Pie, Cell, Tooltip } from 'recharts';
+
+const fatNorm = 54;
+
+const fat = [
+  { name: 'Fat (g)', value: 30 },
+  { name: 'Remained (g)', value: fatNorm - 30 },
+];
+
+const FatPie = () => {
+  return (
+    <div className='text-center'>
+      <span>Fat</span>
+      <PieChart width={260} height={260}>
+        <Pie
+          dataKey='value'
+          startAngle={90}
+          endAngle={-270}
+          data={fat}
+          cx='50%'
+          cy='50%'
+          outerRadius={80}
+          fill='#8884d8'
+          label
+        >
+          <Cell fill='#86efac' />
+          <Cell fill='#bfdbfe' />
+        </Pie>
+        <Tooltip />
+      </PieChart>
+    </div>
+  );
+};
+
+export default FatPie;
--- src/pages/[user]/calories/index.tsx
+++ src/pages/[user]/calories/index.tsx
@@ -1,5 +1,38 @@
+import dynamic from 'next/dynamic';
+
+const DynamicCaloriesPie = dynamic(() => import('@/components/CaloriesPie'), {
+  ssr: false,
+});
+
+const DynamicFatPie = dynamic(() => import('@/components/FatPie'), {
+  ssr: false,
+});
+
+const DynamicProteinPie = dynamic(() => import('@/components/ProteinPie'), {
+  ssr: false,
+});
+
+const DynamicCarbohydratesPie = dynamic(
+  () => import('@/components/CarbohydratesPie'),
+  { ssr: false }
+);
+
 import HomeLayout from '@/layouts/Home';
 
 export default function Calories() {
-  return <HomeLayout>Calories</HomeLayout>;
+  return (
+    <HomeLayout>
+      <div className='flex flex-col items-center justify-start'>
+        <div>
+          <h3 className='text-center text-2xl'>Today's progress</h3>
+          <div className='mt-11 flex'>
+            <DynamicCaloriesPie />
+            <DynamicFatPie />
+            <DynamicProteinPie />
+            <DynamicCarbohydratesPie />
+          </div>
+        </div>
+      </div>
+    </HomeLayout>
+  );
 }
