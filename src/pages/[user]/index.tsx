118275cad695124554a8e825fe69bca5d54727da
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-05-25 19:56:40 +0200

    2

--- src/pages/[user]/index.tsx
+++ src/pages/[user]/index.tsx
@@ -5,6 +5,8 @@ import { useSession } from 'next-auth/react';
 export default function Account() {
   const { data: session } = useSession();
 
+  console.log(session);
+
   if (!session) {
     return (
       <HomeLayout>
@@ -16,14 +18,15 @@ export default function Account() {
   return (
     <HomeLayout>
       Signed in user account info:
-      <p>{`email: ${session.user?.user.email}`}</p>
-      <p>{`username: ${session.user?.user.username}`}</p>
-      <p>{`age: ${session.user?.user.age}`}</p>
-      <p>{`activity_level: ${session.user?.user.activity_level}`}</p>
-      <p>{`id: ${session.user?.user.user_id}`}</p>
-      <p>{`height: ${session.user?.user.height}`}</p>
-      <p>{`weight: ${session.user?.user.weight}`}</p>
-      <p>{`img_url: ${session.user?.user.img_url}`}</p>
+      <p>{`what is it: ${session.user?.user.email}`}</p>
+      {/* <p>{`email: ${session.user?.user.email}`}</p> */}
+      {/* <p>{`username: ${session.user?.user.username}`}</p> */}
+      {/* <p>{`age: ${session.user?.user.age}`}</p> */}
+      {/* <p>{`activity_level: ${session.user?.user.activity_level}`}</p> */}
+      {/* <p>{`id: ${session.user?.user.user_id}`}</p> */}
+      {/* <p>{`height: ${session.user?.user.height}`}</p> */}
+      {/* <p>{`weight: ${session.user?.user.weight}`}</p> */}
+      {/* <p>{`img_url: ${session.user?.user.img_url}`}</p> */}
     </HomeLayout>
   );
 }
