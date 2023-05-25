c3b37a816944e9c418c5808bc816befffb5f9c05
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-05-25 20:02:20 +0200

    finally?

--- src/pages/[user]/index.tsx
+++ src/pages/[user]/index.tsx
@@ -18,15 +18,14 @@ export default function Account() {
   return (
     <HomeLayout>
       Signed in user account info:
-      <p>{`what is it: ${session.user.user.email}`}</p>
-      {/* <p>{`email: ${session.user?.user.email}`}</p> */}
-      {/* <p>{`username: ${session.user?.user.username}`}</p> */}
-      {/* <p>{`age: ${session.user?.user.age}`}</p> */}
-      {/* <p>{`activity_level: ${session.user?.user.activity_level}`}</p> */}
-      {/* <p>{`id: ${session.user?.user.user_id}`}</p> */}
-      {/* <p>{`height: ${session.user?.user.height}`}</p> */}
-      {/* <p>{`weight: ${session.user?.user.weight}`}</p> */}
-      {/* <p>{`img_url: ${session.user?.user.img_url}`}</p> */}
+      <p>{`email: ${session.user.user.email}`}</p>
+      <p>{`username: ${session.user.user.username}`}</p>
+      <p>{`age: ${session.user.user.age}`}</p>
+      <p>{`activity_level: ${session.user.user.activity_level}`}</p>
+      <p>{`id: ${session.user.user.user_id}`}</p>
+      <p>{`height: ${session.user.user.height}`}</p>
+      <p>{`weight: ${session.user.user.weight}`}</p>
+      <p>{`img_url: ${session.user.user.img_url}`}</p>
     </HomeLayout>
   );
 }
