a4616a00716f7b46b5916dcc67d39bf4e640961b
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-05-31 00:58:46 +0200

    no-scrollbar

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -84,7 +84,7 @@ const Home: FC<HomeProps> = ({ children }) => {
           <span className='font-medium'>Sign out</span>
         </button>
       </div>
-      <div className='h-full max-h-full flex-1 overflow-scroll px-10 py-5'>
+      <div className='no-scrollbar h-full max-h-full flex-1 overflow-scroll px-10 py-5'>
         {children}
       </div>
     </section>
