603f536b9ea6fa554f5acb4e9e71cc6c01bf2637
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-06-05 19:52:13 +0200

    getServerSideProps

--- src/pages/[user]/calories/index.tsx
+++ src/pages/[user]/calories/index.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 import { useRouter } from 'next/router';
 
+import { getToken } from 'next-auth/jwt';
+
 import { useSession } from 'next-auth/react';
 
 import AddDaily from '@/components/AddDaily';
@@ -11,7 +13,69 @@ import HomeLayout from '@/layouts/Home';
 
 import { PlusSvg } from '@/assets/svgs';
 
-export default function Calories() {
+import { getTodaysDate, getWeekData } from '@/utils/utils';
+
+export const getServerSideProps = async (context: any) => {
+  const session = await getToken({ req: context.req });
+
+  if (!session) {
+    return {
+      props: {},
+    };
+  }
+
+  const responses = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/calories`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.token}`,
+      },
+    }),
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/calories/consumption`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.token}`,
+      },
+    }),
+  ]);
+
+  const data = await Promise.all(responses.map((res) => res.json()));
+
+  const todaysDonutsData = data[0][getTodaysDate()];
+
+  const weekData = getWeekData(data[0]);
+
+  return {
+    props: {
+      data: {
+        donuts: {
+          current: {
+            calories: todaysDonutsData ? todaysDonutsData.calories : 0,
+            carbohydrates: todaysDonutsData
+              ? todaysDonutsData.carbohydrates
+              : 0,
+            fat: todaysDonutsData ? todaysDonutsData.fat : 0,
+            protein: todaysDonutsData ? todaysDonutsData.protein : 0,
+          },
+          norm: {
+            calories: data[1].calories,
+            carbohydrates: data[1].carbohydrates,
+            fat: data[1].fat,
+            protein: data[1].protein,
+          },
+        },
+        weekCalories: weekData[0],
+        weekCarbohydrates: weekData[1],
+        weekFat: weekData[2],
+        weekProtein: weekData[3],
+      },
+    },
+  };
+};
+
+export default function Calories({ data }: { data: any }) {
   const router = useRouter();
 
   const [addDaily, setAddDaily] = useState(false);
@@ -33,7 +97,7 @@ export default function Calories() {
       ) : (
         <>
           <div className='flex flex-col items-center justify-start gap-8 px-10 py-5'>
-            <Charts token={session.user.token} />
+            <Charts data={data} />
           </div>
           <button
             onClick={() => setAddDaily(true)}
