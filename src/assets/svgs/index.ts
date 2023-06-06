4be4706617a40fb7558f789d4f2a5bd262069350
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-06-06 13:58:54 +0200

    start fridge-page

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -5,3 +5,5 @@ export { default as FridgeSvg } from './fridge.svg';
 export { default as RecipesSvg } from './recipes.svg';
 export { default as PlusSvg } from './plus.svg';
 export { default as DefaultUserImage } from './defaultUserImage.svg';
+export { default as SearchSvg } from './search.svg';
+export { default as DeleteSvg } from './delete.svg';
