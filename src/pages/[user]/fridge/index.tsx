6cb4d14adaf4a10c32370410d89c3604e356a274
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2023-06-07 00:10:36 +0200

    fridge page template

--- src/pages/[user]/fridge/index.tsx
+++ src/pages/[user]/fridge/index.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 import HomeLayout from '@/layouts/Home';
+
 import { DeleteSvg, SearchSvg } from '@/assets/svgs';
 
 export default function Fridge() {
+  const [search, setSearch] = useState('');
+
   const [data, setData] = useState([
     {
       id: 1,
@@ -40,41 +44,100 @@ export default function Fridge() {
       expires: '14/10/2023',
       quantity: 1,
     },
+    {
+      id: 6,
+      name: 'Kabanos',
+      location: 'Fridge',
+      expires: '14/10/2023',
+      quantity: 2,
+    },
+    {
+      id: 7,
+      name: 'Bananas',
+      location: 'Fridge',
+      expires: '13/10/2023',
+      quantity: 3,
+    },
+    {
+      id: 8,
+      name: 'Kiwi',
+      location: 'Table',
+      expires: '14/10/2023',
+      quantity: 5,
+    },
+    {
+      id: 9,
+      name: 'Apple',
+      location: 'Table',
+      expires: '21/10/2023',
+      quantity: 8,
+    },
+    {
+      id: 10,
+      name: 'Sausage',
+      location: 'Table',
+      expires: '20/10/2023',
+      quantity: 5,
+    },
   ]);
+
+  const products = () => {
+    const filteredData = data.filter((item) =>
+      item.name.toLowerCase().includes(search.toLowerCase())
+    );
+
+    return filteredData.length > 0 ? (
+      filteredData.map((item) => {
+        return (
+          <div key={item.id} className='flex justify-between'>
+            <div className='flex w-full transform justify-between rounded-lg border-b-[3px] border-neutral-300 bg-neutral-100 px-5 py-2 text-lg transition-[transform] hover:scale-[1.02]'>
+              <div>
+                <span className='mr-4 cursor-pointer'>{`(${item.quantity})`}</span>
+                <span className='cursor-pointer'>{item.name}</span>
+              </div>
+              <div className='flex w-[55%] justify-between'>
+                <span className='cursor-pointer'>{item.location}</span>
+                <span className='cursor-pointer'>{item.expires}</span>
+              </div>
+            </div>
+            <button
+              onClick={() => {
+                if (
+                  window.confirm(
+                    `Are you sure you want to delete ${item.name} (${item.quantity})?`
+                  )
+                ) {
+                  setData(data.filter((i) => i.id !== item.id));
+
+                  toast.success('Product was successfully deleted.');
+                }
+              }}
+              className='ml-5'
+            >
+              <DeleteSvg />
+            </button>
+          </div>
+        );
+      })
+    ) : (
+      <div className='text-center font-bold'>{`No ${search} in Your fridge :<`}</div>
+    );
+  };
 
   return (
     <HomeLayout>
       <div className='mt-[20px] flex justify-center'>
-        <div className='mb-[10px] flex w-full max-w-[300px] items-center justify-between rounded-lg border border-neutral-200 p-3'>
+        <div className='mb-[5px] flex w-full max-w-[300px] items-center justify-between rounded-lg border border-neutral-200 p-3'>
           <SearchSvg className='scale-[0.65] transform' />
           <input
+            onChange={(e) => setSearch(e.target.value)}
             className='w-[237px]'
             type='text'
-            placeholder='what do you want...'
+            placeholder='what do you want to find...'
           />
         </div>
       </div>
-      <div className='flex h-full w-full flex-col gap-5 px-10 py-5'>
-        {data.map((item) => {
-          return (
-            <div key={item.id} className='flex justify-between'>
-              <div className='flex w-full transform justify-between rounded-lg border-b-[3px] border-neutral-300 bg-neutral-100 px-5 py-2 text-lg transition-[transform] hover:scale-[1.02]'>
-                <div>
-                  <span className='mr-4 cursor-pointer'>{`(${item.quantity})`}</span>
-                  <span className='cursor-pointer'>{item.name}</span>
-                </div>
-                <div className='flex w-[55%] justify-between'>
-                  <span className='cursor-pointer'>{item.location}</span>
-                  <span className='cursor-pointer'>{item.expires}</span>
-                </div>
-              </div>
-              <button className='ml-5'>
-                <DeleteSvg />
-              </button>
-            </div>
-          );
-        })}
-      </div>
+      <div className='flex w-full flex-col gap-5 px-10 py-5'>{products()}</div>
     </HomeLayout>
   );
 }
